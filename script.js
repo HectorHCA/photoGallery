2b74e494fee1f6b487bdb5e1247aa16b52dbaa92
HectorHCA <83581041+HectorHCA@users.noreply.github.com>
2023-04-28 20:34:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -25,15 +25,19 @@
 
     
 
+           
             function openOverlayH(imageUrl) {
                 var overlay = document.getElementById('overlayH');
                 var overlayImage = document.getElementById('overlayImageH');
                 var closeButton = document.getElementById('closeButton');
+            
+                overlayImage.onload = function() {  // agregar evento load a la imagen del overlay
+                    overlay.style.display = 'block';
+                    document.body.style.overflow = 'hidden';
+                }
                 
                 overlayImage.src = imageUrl;
-                overlay.style.display = 'block';
-                document.body.style.overflow = 'hidden';
-                
+            
                 closeButton.onclick = function() {
                     overlay.style.display = 'none';
                     document.body.style.overflow = 'auto';
@@ -42,34 +46,36 @@
                 overlay.onclick = function() {
                     overlay.style.display = 'none';
                     document.body.style.overflow = 'auto';
-                }
-                
+                }                
             }
+            
+
 
             function openOverlayV(imageUrl) {
                 var overlay = document.getElementById('overlayV');
                 var overlayImage = document.getElementById('overlayImageV');
                 var closeButton = document.getElementById('closeButton');
+            
+                overlayImage.onload = function() {  // agregar evento load a la imagen del overlay
+                    overlay.style.display = 'block';
+                    document.body.style.overflow = 'hidden';
+                }
                 
                 overlayImage.src = imageUrl;
-                overlay.style.display = 'block';
-                document.body.style.overflow = 'hidden';
-                
-                
+            
                 closeButton.onclick = function() {
                     overlay.style.display = 'none';
                     document.body.style.overflow = 'auto';
                 }
                 
-                
                 overlay.onclick = function() {
                     overlay.style.display = 'none';
                     document.body.style.overflow = 'auto';
-                }
-
-                
-                
+                }                
             }
+            
+
+
 
 
 
